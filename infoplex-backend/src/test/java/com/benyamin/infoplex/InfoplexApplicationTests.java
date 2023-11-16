523f21b101ecba266c30532ca2f7173213d36434
Benyamin Radmard <b.radmard77@gmail.com>
2023-11-16 21:12:37 +0100

    Backed integration
    + Added Blog Entity
    + Configured DB
    + Added controllers
    + Added BlogRepository

---
+++ infoplex-backend/src/test/java/com/benyamin/infoplex/InfoplexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.benyamin.infoplex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfoplexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
